83ce48be17c8b467bee0fc78d01122419f46891e
Pedro Monteiro <pedrosergiomaia@gmail.com>
2022-09-04 00:03:08 -0300

    update
    atualizaçao para incluir tooltip e possibilidade do usuário digitar os binários através do teclado.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+
 const inputBinarios = document.getElementById('input-binario');
 const inputDecimais = document.getElementById('input-decimal');
-const btn = document.getElementById('btn-converter');
+const btnConvert = document.getElementById('btn-convert');
+const btnReset = document.getElementById('btn-reset');
+
 const btnZero = document.getElementById('btn-zero');
 const btnOne = document.getElementById('btn-one');
-
+// const testeKey = document.getElementById('input-binario');
+const bin = document.querySelector('.bin');
+const dec = document.querySelector('.dec');
 
 btnZero.addEventListener("click", function(){
     if(inputDecimais.value==0){
@@ -25,7 +30,6 @@ btnOne.addEventListener("click", function(){
 });
 
 
-
 function verificaBinarios() {
 
     let binario = inputBinarios.value;
@@ -45,17 +49,46 @@ function converteBinario() {
     return somaString;
 }
 
-btn.addEventListener('click', function() {
-
+btnConvert.addEventListener('click', function() {
     if (verificaBinarios()){
         // mensagem.classList.remove('on');
         
         inputDecimais.value = converteBinario();
     } else {
-        mensagem.classList.add('on');
+        // mensagem.classList.add('on');
         inputDecimais.value = "";
     }
     
+});
+
+btnReset.addEventListener('click', function(){
+    inputBinarios.value = "";
+    inputDecimais.value = "";
+    bin.classList.remove('on');
+});
+
+// IMPEDE QUE O USUARIO DIGITE QUALQUER NÚMERO DIFERENTE DE 0 OU 1
+// testeKey.addEventListener("keydown", function(event)
+// {       if (event.key!=0 && event.key!=1){
+//         event.preventDefault()
+              
+// });
+
+// Caso o usuário digite um número diferente de 0 ou 1 adiciona classe ON
+// testeKey.addEventListener("keydown", function(event)
+// {       if (event.key!=0 && event.key!=1){
+//         bin.classList.add('on');       
+// });
+
+inputBinarios.addEventListener('input', function(){
+    let binario = inputBinarios.value;
+    let tamanhoBinarios = binario.split('').filter(x=>x==0 || x==1 ).length;
+
+    if(!verificaBinarios()){
+        bin.classList.add('on');
+    } else {
+        bin.classList.remove('on');
+    }
 
 });
 
